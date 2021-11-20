d1dd6bce56ec91add22d2f696e16e35a0135f066
shengrihui <1120058252@qq.com>
2021-11-20 15:58:14 +0800

    model 11 , mycosin , modify sava

--- CCF_BoatLine.py
+++ CCF_BoatLine.py
@@ -7,51 +7,55 @@ Created on  2021/10/26 20:16
 
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
 import os
 import csv
 import pandas as pd
 from collections import defaultdict
 from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
+from MyCosineAnnealingWarmRestarts import MyCosineAnnealingWarmRestarts as MyCos
 
 
 class Config():
-    # 输入维度4，精度纬度速度角度
-    input_size = 4
-    # 输出维度2，精度纬度
-    out_size = 2
+    normalization = {
+        'lat': {'min': 30.335862, 'max': 30.583262},
+        'lon': {'min': 121.652935, 'max': 121.881350},
+        'Cog': {'min': 0.000000, 'max': 359.000000},
+        'Sog': {'min': 1.100000, 'max': 36.000000}
+    }
+
+    input_size = 4  # 输入维度4，精度纬度速度角度
+    out_size = 2  # 输出维度2，精度纬度
     batch_size = 4
     hidden_size = 30
     num_layers = 3
+    input_len = 12  # 输入长度
     out_len = 6  # 预测长度10
-    input_len = 15  # 输入长度
     data_len = out_len + input_len
 
-    normalization = {
-        'lat': {'min': 30.335862,'max': 30.583262},
-        'lon': {'min': 121.652935,'max': 121.881350},
-        'Cog': {'min': 0.000000,'max': 359.000000},
-        'Sog': {'min': 1.100000,'max': 36.000000}
-    }
-
-    lr = 0.0002
+    lr = 0.01
     momentum = 0.9
-    start_epoch = 453
-    epochs = 10
-    save_dir = '10'
+    start_epoch = 200
+    epochs = 100
+    save_dir = '11'
     
-    pt_path = f'{save_dir}/model_34.pt'
+    model_path = f'{save_dir}/model/'
+    best_model = f'{model_path}/model_34.pt'
     logs = f'{save_dir}/logs/'
-    best = 0.983561
-    pred_best = 0.979899
+    result_path = f'{save_dir}/result/'
+    test_best = 0.9903486290068979
+    pred_best = 0.9777078582010242
 
 
 config = Config()
 if not os.path.exists(config.save_dir):
     os.mkdir(config.save_dir)
     os.makedirs(config.logs)
+    os.makedirs(config.model_path)
+    os.makedirs(config.result_path)
+
 
 # 数据归一化和返回数据
 class Data_Normalization():
@@ -59,22 +63,22 @@ class Data_Normalization():
     def normalization(data, data_name):
         m1 = config.normalization[data_name]['min']
         m2 = config.normalization[data_name]['max']
-        return (data-m1)/(m2-m1)
-
+        return (data - m1) / (m2 - m1)
+    
     @staticmethod
     def n2data(out, data_name):
         m1 = config.normalization[data_name]['min']
         m2 = config.normalization[data_name]['max']
-        return out*(m2-m1)+m1
+        return out * (m2 - m1) + m1
 
 
 class CCFData(Dataset):
     _data = []
     _pred = []
-
+    
     @classmethod
     def read_csv(cls):
-        train_csv = pd.read_csv('train.csv')
+        train_csv = pd.read_csv('data/train.csv')
         mmsi = train_csv['mmsi']
         lat = train_csv['lat']
         lon = train_csv['lon']
@@ -85,7 +89,7 @@ class CCFData(Dataset):
         for i in range(len(mmsi)):
             mmsi_dict[mmsi[i]].append(
                 [lat[i], lon[i], sog[i], cog[i], time[i]])
-
+        
         def spllit_each_mmsi(data, m=0):
             data_name = ['lat', 'lon', 'Sog', 'Cog']
             ret = []
@@ -100,16 +104,17 @@ class CCFData(Dataset):
                         break
                 cell_in = [[Data_Normalization.normalization(data[k][j], data_name[j]) for j in range(4)] for k in
                            range(i, i + config.input_len)]
-                cell_out = [[Data_Normalization.normalization(data[j][0], 'lat'), Data_Normalization.normalization(data[j][1], 'lon')]
+                cell_out = [[Data_Normalization.normalization(data[j][0], 'lat'),
+                             Data_Normalization.normalization(data[j][1], 'lon')]
                             for j in range(i + config.input_len, i + config.data_len)]
-
+                
                 if m < 27:
                     ret.append([cell_in, cell_out])
                 elif m >= 27:
                     ret.append([cell_in, cell_out, m])
                 i += 1
             return ret
-
+        
         for m in mmsi_dict:
             if m < 27:
                 cls._data.extend(spllit_each_mmsi(mmsi_dict[m]))
@@ -118,7 +123,7 @@ class CCFData(Dataset):
                     mmsi_dict[m][:-config.data_len]))
                 cls._pred.extend(spllit_each_mmsi(
                     mmsi_dict[m][-config.data_len:], m))
-
+    
     def __init__(self, mode='train'):
         super(CCFData, self).__init__()
         if self._data == []:
@@ -131,13 +136,13 @@ class CCFData(Dataset):
             self.data = self._data[int(0.8 * self.n):]
         elif mode == 'pred':
             self.data = self._pred
-
+    
     def __getitem__(self, idx):
         if self.mode != 'pred':
             return torch.tensor(self.data[idx][0]).double(), torch.tensor(self.data[idx][1]).double()
         else:
             return torch.tensor(self.data[idx][0]).double(), torch.tensor(self.data[idx][1]).double(), self.data[idx][2]
-
+    
     def __len__(self):
         return len(self.data)
 
@@ -158,11 +163,11 @@ class Model(nn.Module):
             batch_first=True
         )
         self.fc = nn.Sequential(
-            nn.Linear(self.input_len * self.hidden_size, 32),
+            nn.Linear(self.input_len * self.hidden_size, 60),
             nn.Sigmoid(),
-            nn.Linear(32, self.out_len * self.out_size)
+            nn.Linear(60, self.out_len * self.out_size)
         )
-
+    
     # def forward(self, x, hidden):
     # out, (h_n, c_n) = self.lstm(x, hidden)
     def forward(self, x):
@@ -173,7 +178,7 @@ class Model(nn.Module):
         out = self.fc(out)
         out = out.reshape(bs, self.out_len, -1)
         return out
-
+    
     # def init_h_c(self, bs=config.batch_size):
     #     h = torch.zeros(self.num_layers, bs, self.hidden_size)
     #     c = torch.zeros(self.num_layers, bs, self.hidden_size)
@@ -197,18 +202,18 @@ class My_loss(nn.Module):
         self.score = 0
         self.num = 0
         self.total_loss = 0
-
+    
     def forward(self, y_hat, y):
         for y1, y2 in zip(y_hat, y):
             loss = F.mse_loss(y, y_hat)
             self.num += 1
             self.total_loss += loss.item()
             if self.num % 60 == 0:
-                s = 1/(self.total_loss+1)
+                s = 1 / (self.total_loss + 1)
                 self.total_loss = 0
                 self.s.append(s)
         if self.s != []:
-            self.score = sum(self.s)/len(self.s)
+            self.score = sum(self.s) / len(self.s)
             return self.score
         else:
             return 0
@@ -218,15 +223,18 @@ criterion = nn.MSELoss(reduction='sum')
 optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
 # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
 #     optimizer, 'max', 0.1, 10)
-#scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[15,35,60], gamma=0.4)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=3,T_mult=2,eta_min=0)
+# scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[15,35,60], gamma=0.4)
+# scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=3,T_mult=2,eta_min=0)
+scheduler = MyCos(optimizer, T_0=5, T_mult=2,factor=0.9, eta_min=0.000001)
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # device = torch.device('cpu')
+
 model.double()
 model.to(device)
 criterion.to(device)
-if os.path.exists(config.pt_path):
-    model.load_state_dict(torch.load(config.pt_path))
+if os.path.exists(config.best_model):
+    model.load_state_dict(torch.load(config.best_model))
 
 
 def train(epoch):
@@ -247,8 +255,7 @@ def train(epoch):
         optimizer.step()
         tbar.set_description('Epoch %d : loss:%.6f total_loss:%.6f' % (
             epoch, loss.item(), total_loss))
-    if epoch > 0:
-        writer.add_scalar('train_loss', total_loss, epoch)
+    writer.add_scalar('train_loss', total_loss, epoch)
 
 
 def test(epoch):
@@ -277,7 +284,7 @@ def pred(best_pred, epoch):
         with open(path, "w", newline='') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerows(out)
-
+    
     pred_loader = DataLoader(dataset=pred_data,
                              shuffle=False,
                              drop_last=False,
@@ -295,17 +302,17 @@ def pred(best_pred, epoch):
             total_loss += loss.item()
             mse = total_loss / config.out_len
             score = 1 / (mse + 1)
-            tbar.set_description('Pred score:%f...'%score)
+            tbar.set_description('Pred score:%f...' % score)
             for lat, lon in out:
                 lat = str(Data_Normalization.n2data(lat.item(), 'lat'))
                 lon = str(Data_Normalization.n2data(lon.item(), 'lon'))
                 out_csv.append([str(mmsi.item()), lat, lon])
-    #print('pred score：', score)
+    # print('pred score：', score)
     writer.add_scalar('pred_score', score, epoch)
-    write_csv(out_csv, f'{config.save_dir}/result_{epoch}_{score}.csv')
+    write_csv(out_csv, f'{config.result_path}/result_{epoch}_{score}.csv')
     if score > best_pred:
         print('save result..')
-        write_csv(out, f'{config.save_dir}/result_{epoch}.csv')
+        write_csv(out, f'{config.result_path}/result{epoch}.csv')
     return score
 
 
@@ -314,31 +321,36 @@ if __name__ == '__main__':
     test_data = CCFData('test')
     pred_data = CCFData('pred')
     writer = SummaryWriter(config.logs)
-
     
-    exit(0)
-    best = config.best
-    best_pred = config.pred_best
-    p_score=best_pred
+
+    test_best = config.test_best
+    pred_best = config.pred_best
+    p_score = pred_best
     for epoch in range(config.start_epoch, config.start_epoch + config.epochs):
         # break
         writer.add_scalar('lr', optimizer.state_dict()[
-                          'param_groups'][0]['lr'], epoch)
+            'param_groups'][0]['lr'], epoch)
         train(epoch)
-        if (epoch - config.start_epoch) % 1 == 0:
-            score = test(epoch)
-            if score > best:
-                best = score
-                torch.save(model.state_dict(), f'{config.save_dir}/model_{epoch}.pt')
-                print("Save model...")
-        p= pred(p_score, epoch)
-        if p>p_score:
-            p_score=p
         
-        #scheduler.step(p_score)
-        scheduler.step()
-    print('best', best)
-    print('best_pred',p_score)
+        is_save =False
+        test_score = test(epoch)
+        if test_score > test_best:
+            test_best = test_score
+            is_save=True
+       
+        p = pred(p_score, epoch)
+        if p > p_score:
+            p_score = p
+            is_save=True
+        
+        if is_save:
+            torch.save(model.state_dict(), f'{config.model_path}/model_{epoch}.pt')
+            print("Save model...")
 
-    torch.save(model.state_dict(), f'{config.save_dir}/model_last.pt')
+        # scheduler.step(p_score)
+        scheduler.step()
+    print('test_best', test_best)
+    print('best_pred', p_score)
+    
+    torch.save(model.state_dict(), f'{config.model_path}/model_last.pt')
     print("Save model...")
---
+++ MyCosineAnnealingWarmRestarts.py
@@ -0,0 +1,317 @@
+# -*- coding: utf-8 -*-
+"""
+Created on  2021/11/19 19:22
+
+@author: shengrihui
+"""
+# import types
+# import math
+# from torch._six import inf
+from functools import wraps
+import warnings
+import weakref
+# from collections import Counter
+# from bisect import bisect_right
+
+import torch.optim.optimizer
+from torch.optim.lr_scheduler import *
+
+
+class _LRScheduler(object):
+    
+    def __init__(self, optimizer, last_epoch=-1, verbose=False):
+        
+        # Attach optimizer
+        if not isinstance(optimizer, Optimizer):
+            raise TypeError('{} is not an Optimizer'.format(
+                type(optimizer).__name__))
+        self.optimizer = optimizer
+        
+        # Initialize epoch and base learning rates
+        if last_epoch == -1:
+            for group in optimizer.param_groups:
+                group.setdefault('initial_lr', group['lr'])
+        else:
+            for i, group in enumerate(optimizer.param_groups):
+                if 'initial_lr' not in group:
+                    raise KeyError("param 'initial_lr' is not specified "
+                                   "in param_groups[{}] when resuming an optimizer".format(i))
+        self.base_lrs = [group['initial_lr'] for group in optimizer.param_groups]
+        self.last_epoch = last_epoch
+        
+        # Following https://github.com/pytorch/pytorch/issues/20124
+        # We would like to ensure that `lr_scheduler.step()` is called after
+        # `optimizer.step()`
+        def with_counter(method):
+            if getattr(method, '_with_counter', False):
+                # `optimizer.step()` has already been replaced, return.
+                return method
+            
+            # Keep a weak reference to the optimizer instance to prevent
+            # cyclic references.
+            instance_ref = weakref.ref(method.__self__)
+            # Get the unbound method for the same purpose.
+            func = method.__func__
+            cls = instance_ref().__class__
+            del method
+            
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                instance = instance_ref()
+                instance._step_count += 1
+                wrapped = func.__get__(instance, cls)
+                return wrapped(*args, **kwargs)
+            
+            # Note that the returned function here is no longer a bound method,
+            # so attributes like `__func__` and `__self__` no longer exist.
+            wrapper._with_counter = True
+            return wrapper
+        
+        self.optimizer.step = with_counter(self.optimizer.step)
+        self.optimizer._step_count = 0
+        self._step_count = 0
+        self.verbose = verbose
+        
+        self.step()
+    
+    def state_dict(self):
+        """Returns the state of the scheduler as a :class:`dict`.
+
+        It contains an entry for every variable in self.__dict__ which
+        is not the optimizer.
+        """
+        return {key: value for key, value in self.__dict__.items() if key != 'optimizer'}
+    
+    def load_state_dict(self, state_dict):
+        """Loads the schedulers state.
+
+        Args:
+            state_dict (dict): scheduler state. Should be an object returned
+                from a call to :meth:`state_dict`.
+        """
+        self.__dict__.update(state_dict)
+    
+    def get_last_lr(self):
+        """ Return last computed learning rate by current scheduler.
+        """
+        return self._last_lr
+    
+    def get_lr(self):
+        # Compute learning rate using chainable form of the scheduler
+        raise NotImplementedError
+    
+    def print_lr(self, is_verbose, group, lr, epoch=None):
+        """Display the current learning rate.
+        """
+        if is_verbose:
+            if epoch is None:
+                print('Adjusting learning rate'
+                      ' of group {} to {:.4e}.'.format(group, lr))
+            else:
+                print('Epoch {:5d}: adjusting learning rate'
+                      ' of group {} to {:.4e}.'.format(epoch, group, lr))
+    
+    def step(self, epoch=None):
+        # Raise a warning if old pattern is detected
+        # https://github.com/pytorch/pytorch/issues/20124
+        if self._step_count == 1:
+            if not hasattr(self.optimizer.step, "_with_counter"):
+                warnings.warn("Seems like `optimizer.step()` has been overridden after learning rate scheduler "
+                              "initialization. Please, make sure to call `optimizer.step()` before "
+                              "`lr_scheduler.step()`. See more details at "
+                              "https://pytorch.org/docs/stable/optim.html#how-to-adjust-learning-rate", UserWarning)
+            
+            # Just check if there were two first lr_scheduler.step() calls before optimizer.step()
+            elif self.optimizer._step_count < 1:
+                warnings.warn("Detected call of `lr_scheduler.step()` before `optimizer.step()`. "
+                              "In PyTorch 1.1.0 and later, you should call them in the opposite order: "
+                              "`optimizer.step()` before `lr_scheduler.step()`.  Failure to do this "
+                              "will result in PyTorch skipping the first value of the learning rate schedule. "
+                              "See more details at "
+                              "https://pytorch.org/docs/stable/optim.html#how-to-adjust-learning-rate", UserWarning)
+        self._step_count += 1
+        
+        class _enable_get_lr_call:
+            
+            def __init__(self, o):
+                self.o = o
+            
+            def __enter__(self):
+                self.o._get_lr_called_within_step = True
+                return self
+            
+            def __exit__(self, type, value, traceback):
+                self.o._get_lr_called_within_step = False
+        
+        with _enable_get_lr_call(self):
+            if epoch is None:
+                self.last_epoch += 1
+                values = self.get_lr()
+            else:
+                warnings.warn(EPOCH_DEPRECATION_WARNING, UserWarning)
+                self.last_epoch = epoch
+                if hasattr(self, "_get_closed_form_lr"):
+                    values = self._get_closed_form_lr()
+                else:
+                    values = self.get_lr()
+        
+        for i, data in enumerate(zip(self.optimizer.param_groups, values)):
+            param_group, lr = data
+            param_group['lr'] = lr
+            self.print_lr(self.verbose, i, lr, epoch)
+        
+        self._last_lr = [group['lr'] for group in self.optimizer.param_groups]
+
+
+class MyCosineAnnealingWarmRestarts(_LRScheduler):
+    r"""Set the learning rate of each parameter group using a cosine annealing
+    schedule, where :math:`\eta_{max}` is set to the initial lr, :math:`T_{cur}`
+    is the number of epochs since the last restart and :math:`T_{i}` is the number
+    of epochs between two warm restarts in SGDR:
+
+    .. math::
+        \eta_t = \eta_{min} + \frac{1}{2}(\eta_{max} - \eta_{min})\left(1 +
+        \cos\left(\frac{T_{cur}}{T_{i}}\pi\right)\right)
+
+    When :math:`T_{cur}=T_{i}`, set :math:`\eta_t = \eta_{min}`.
+    When :math:`T_{cur}=0` after restart, set :math:`\eta_t=\eta_{max}`.
+
+    It has been proposed in
+    `SGDR: Stochastic Gradient Descent with Warm Restarts`_.
+
+    Args:
+        optimizer (Optimizer): Wrapped optimizer.
+        T_0 (int): Number of iterations for the first restart.
+        T_mult (int, optional): A factor increases :math:`T_{i}` after a restart. Default: 1.
+        eta_min (float, optional): Minimum learning rate. Default: 0.
+        last_epoch (int, optional): The index of last epoch. Default: -1.
+        verbose (bool): If ``True``, prints a message to stdout for
+            each update. Default: ``False``.
+
+    .. _SGDR\: Stochastic Gradient Descent with Warm Restarts:
+        https://arxiv.org/abs/1608.03983
+    """
+    
+    def __init__(self, optimizer, T_0, factor=0.8, T_mult=1, eta_min=0, last_epoch=-1, verbose=False):
+        if T_0 <= 0 or not isinstance(T_0, int):
+            raise ValueError("Expected positive integer T_0, but got {}".format(T_0))
+        if T_mult < 1 or not isinstance(T_mult, int):
+            raise ValueError("Expected integer T_mult >= 1, but got {}".format(T_mult))
+        self.T_0 = T_0
+        self.T_i = T_0
+        self.T_mult = T_mult
+        self.eta_min = eta_min
+        self.factor = factor
+        super(MyCosineAnnealingWarmRestarts, self).__init__(optimizer, last_epoch, verbose)
+        
+        self.T_cur = self.last_epoch
+    
+    def get_lr(self):
+        if not self._get_lr_called_within_step:
+            warnings.warn("To get the last learning rate computed by the scheduler, "
+                          "please use `get_last_lr()`.", UserWarning)
+        
+        return [self.eta_min + (base_lr - self.eta_min) * (1 + math.cos(math.pi * self.T_cur / self.T_i)) / 2
+                for base_lr in self.base_lrs]
+    
+    def step(self, epoch=None):
+        """Step could be called after every batch update
+
+        Example:
+            >>> scheduler = CosineAnnealingWarmRestarts(optimizer, T_0, T_mult)
+            >>> iters = len(dataloader)
+            >>> for epoch in range(20):
+            >>>     for i, sample in enumerate(dataloader):
+            >>>         inputs, labels = sample['inputs'], sample['labels']
+            >>>         optimizer.zero_grad()
+            >>>         outputs = net(inputs)
+            >>>         loss = criterion(outputs, labels)
+            >>>         loss.backward()
+            >>>         optimizer.step()
+            >>>         scheduler.step(epoch + i / iters)
+
+        This function can be called in an interleaved way.
+
+        Example:
+            >>> scheduler = CosineAnnealingWarmRestarts(optimizer, T_0, T_mult)
+            >>> for epoch in range(20):
+            >>>     scheduler.step()
+            >>> scheduler.step(26)
+            >>> scheduler.step() # scheduler.step(27), instead of scheduler(20)
+        """
+        
+        if epoch is None and self.last_epoch < 0:
+            epoch = 0
+        
+        if epoch is None:
+            epoch = self.last_epoch + 1
+            self.T_cur = self.T_cur + 1
+            if self.T_cur >= self.T_i:
+                self.T_cur = self.T_cur - self.T_i
+                self.T_i = self.T_i * self.T_mult
+                self.base_lrs = [l * self.factor for l in self.base_lrs]
+        else:
+            if epoch < 0:
+                raise ValueError("Expected non-negative epoch, but got {}".format(epoch))
+            if epoch >= self.T_0:
+                if self.T_mult == 1:
+                    self.T_cur = epoch % self.T_0
+                else:
+                    n = int(math.log((epoch / self.T_0 * (self.T_mult - 1) + 1), self.T_mult))
+                    self.T_cur = epoch - self.T_0 * (self.T_mult ** n - 1) / (self.T_mult - 1)
+                    self.T_i = self.T_0 * self.T_mult ** (n)
+            else:
+                self.T_i = self.T_0
+                self.T_cur = epoch
+        self.last_epoch = math.floor(epoch)
+        
+        class _enable_get_lr_call:
+            
+            def __init__(self, o):
+                self.o = o
+            
+            def __enter__(self):
+                self.o._get_lr_called_within_step = True
+                return self
+            
+            def __exit__(self, type, value, traceback):
+                self.o._get_lr_called_within_step = False
+                return self
+        
+        with _enable_get_lr_call(self):
+            for i, data in enumerate(zip(self.optimizer.param_groups, self.get_lr())):
+                param_group, lr = data
+                param_group['lr'] = lr
+                self.print_lr(self.verbose, i, lr, epoch)
+        
+        self._last_lr = [group['lr'] for group in self.optimizer.param_groups]
+
+
+if __name__ == '__main__':
+    class Model(torch.nn.Module):
+        def __init__(self):
+            super(Model, self).__init__()
+            self.linear = torch.nn.Linear(5, 1)
+        
+        def forward(self, x):
+            return 1
+    
+    
+    model = Model()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    
+    scheduler = MyCosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=2, eta_min=0)
+    
+    from matplotlib import pyplot as plt
+    
+    lr_list = []
+    Epoch = 10 + 20 + 40
+    for epoch in range(Epoch):
+        optimizer.step()
+        scheduler.step()
+        l = optimizer.state_dict()['param_groups'][0]['lr']
+        lr_list.append(l)
+        print(epoch, l)
+    
+    plt.plot(range(Epoch), lr_list)
+    plt.show()
